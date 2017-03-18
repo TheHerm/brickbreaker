f2d7c8442c9820933f2aabb6569d10564d01fed9
Alan H <alan.herman@outlook.com>
2017-03-18 16:26:13 -0500

    bricks and working paddle

--- client/canvaslogic.js
+++ client/canvaslogic.js
@@ -5,9 +5,44 @@ export const fillCanvas = function(){
 
   let xPos = canvas.width / 2;
   let yPos = canvas.height - 30;
-  let xMov = -2;
-  let yMov = -2;
+  let xMov = -5;
+  let yMov = -5;
   let ballRadius = 10;
+  let paddleHeight = 10;
+  let paddleWidth = 75;
+  let paddleX = (canvas.width-paddleWidth)/2;
+  let rightPressed = false;
+  let leftPressed = false;
+  let brickRowCount = 3;
+  let brickColumnCount = 5;
+  let brickWidth = 75;
+  let brickHeight = 20;
+  let brickPadding = 10;
+  let brickOffsetTop = 30;
+  let brickOffsetLeft = 30;
+  let bricks = [];
+  for(let c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+  }
+
+  function drawBricks() {
+    for(let c=0; c<brickColumnCount; c++) {
+      for(let r=0; r<brickRowCount; r++) {
+        let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+        let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(bricks[c][r].x, bricks[c][r].y, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
 
   let drawBall = function(){
     ctx.beginPath();
@@ -17,15 +52,56 @@ export const fillCanvas = function(){
     ctx.closePath();
   }
 
-  let draw = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    xPos += xMov;
-    yPos += yMov;
-    if((yPos-ballRadius+2) + yMov < 0 || (yPos+ballRadius-2) + yMov > canvas.height) yMov = -yMov;
-    if((xPos-ballRadius+2) + xMov < 0 || (xPos+ballRadius-2) + xMov > canvas.width) xMov = -xMov;
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
   }
 
-  setInterval(draw, (1000 / 60));
+  function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+  }
+
+  let draw = function() {
+    xPos += xMov;
+    yPos += yMov;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    if((xPos-ballRadius+2) + xMov < 0 || (xPos+ballRadius-2) + xMov > canvas.width)   xMov = -xMov;
+    if((yPos-ballRadius+2) + yMov < 0)   yMov = -yMov;
+    if((yPos+ballRadius-2) > canvas.height) {
+      if(xPos > paddleX && xPos < paddleX + paddleWidth)   yMov = -yMov;
+      else {
+        alert("GAME OVER");
+        clearInterval(frameInterval);
+        return;
+      }
+    }
+    if(rightPressed && paddleX < canvas.width-paddleWidth)   paddleX += 7;
+    else if(leftPressed && paddleX > 0)   paddleX -= 7;
+  }
+
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+
+  let frameInterval = setInterval(draw, (1000 / 60));
 
 }
