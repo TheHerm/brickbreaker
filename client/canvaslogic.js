6b2ab03e1c5d64e78c2f96d9d3e37924d32a9faa
Alan H <alan.herman@outlook.com>
2017-04-07 16:09:38 -0500

    done with brick colission detection

--- client/canvaslogic.js
+++ client/canvaslogic.js
@@ -4,8 +4,8 @@ export const FillCanvas = function(){
   this.ctx = this.canvas.getContext('2d');
   this.xPos = this.canvas.width / 2;
   this.yPos = this.canvas.height - 30;
-  this.xMov = 1;
-  this.yMov = -1;
+  this.xMov = 3;
+  this.yMov = -3;
   this.ballRadius = 10;
   this.paddleHeight = 10;
   this.paddleWidth = 75;
@@ -24,14 +24,14 @@ export const FillCanvas = function(){
   this.lives = 3;
   this.endGame = false;
   this.edges = {
-    top: new Array(21),
-    left: new Array(21),
-    right: new Array(21),
-    bottom: new Array(21),
-    topLeft: [],
-    topRight: [],
-    bottomRight: [],
-    bottomLeft: []
+    left: new Array(21).fill([]),
+    right: new Array(21).fill([]),
+    bottom: new Array(21).fill([]),
+    top: new Array(21).fill([]),
+    topLeft: new Array(3).fill([]),
+    topRight: new Array(3).fill([]),
+    bottomRight: new Array(3).fill([]),
+    bottomLeft: new Array(3).fill([])
   };
   this.startGame = function(){
     for(let c=0; c<this.brickColumnCount; c++) {
@@ -43,7 +43,47 @@ export const FillCanvas = function(){
     // document.addEventListener("keydown", this.keyDownHandler, false);
     // document.addEventListener("keyup", this.keyUpHandler, false);
     document.addEventListener("mousemove", this.mouseMoveHandler, false);
+    this.setBallPixelArray(this.xPos, this.yPos);
     this.animate();
+  }
+  this.setBallPixelArray = function(x, y){
+    let xTransformLeft = x - (1+this.ballRadius / 2);
+    let xTransformRight = x + (1+this.ballRadius / 2);
+    let yTransformDown = y + (1+this.ballRadius / 2);
+    let yTransformUp = y - (1+this.ballRadius / 2);
+    this.edges.topLeft[0] = [xTransformLeft-1, yTransformUp+1];
+    this.edges.topLeft[1] = [xTransformLeft, yTransformUp];
+    this.edges.topLeft[2] = [xTransformLeft+1, yTransformUp-1];
+
+    this.edges.bottomLeft[0] = [xTransformLeft-1, yTransformDown-1];
+    this.edges.bottomLeft[1] = [xTransformLeft, yTransformDown];
+    this.edges.bottomLeft[2] = [xTransformLeft+1, yTransformDown+1];
+
+    this.edges.bottomRight[0] = [xTransformRight+1, yTransformDown-1];
+    this.edges.bottomRight[1] = [xTransformRight, yTransformDown];
+    this.edges.bottomRight[2] = [xTransformRight-1, yTransformDown+1];
+
+    this.edges.topRight[0] = [xTransformRight-1, yTransformUp-1];
+    this.edges.topRight[1] = [xTransformRight, yTransformUp];
+    this.edges.topRight[2] = [xTransformRight+1, yTransformUp+1];
+
+    for(let i = -3; i<4; i++){
+      this.edges.bottom[i+3] = [(x+i), ( y+(this.ballRadius-2) )];
+      this.edges.bottom[i+10] = [(x+i), ( y+(this.ballRadius-3) )];
+      this.edges.bottom[i+17] = [(x+i), ( y+(this.ballRadius-4) )];
+
+      this.edges.top[i+3] = [(x+i), ( y-(this.ballRadius-2) )];
+      this.edges.top[i+10] = [(x+i), ( y-(this.ballRadius-3) )];
+      this.edges.top[i+17] = [(x+i), ( y-(this.ballRadius-4) )];
+
+      this.edges.right[i+3] = [( x+(this.ballRadius-2) ), (y+i)];
+      this.edges.right[i+10] = [( x+(this.ballRadius-3) ), (y+i)];
+      this.edges.right[i+17] = [( x+(this.ballRadius-4) ), (y+i)];
+
+      this.edges.left[i+3] = [( x-(this.ballRadius-2) ), (y+i)];
+      this.edges.left[i+10] = [( x-(this.ballRadius-3) ), (y+i)];
+      this.edges.left[i+17] = [( x-(this.ballRadius-4) ), (y+i)];
+    }
   }
   this.stopGame = function(){
     this.endGame = true;
@@ -55,15 +95,27 @@ export const FillCanvas = function(){
     }
   }
   this.drawLives = function() {
-    this.ctx.font = "16px Arial";
+    this.ctx.beginPath();
+    this.ctx.rect(this.canvas.width-70, 0, 70, 20);
     this.ctx.fillStyle = "#0095DD";
-    this.ctx.fillText("Lives: " + this.lives, this.canvas.width-65, 20);
+    this.ctx.fill();
+    this.ctx.closePath();
+    this.ctx.beginPath();
+    this.ctx.font = "16px Arial";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("Lives: " + this.lives, this.canvas.width-62, 16);
+    this.ctx.closePath();
   }
   this.drawScore = function() {
     this.ctx.beginPath();
-    this.ctx.font = "16px Arial";
+    this.ctx.rect(0, 0, 75, 20);
     this.ctx.fillStyle = "#0095DD";
-    this.ctx.fillText("Score: " + this.score, 8, 20);
+    this.ctx.fill();
+    this.ctx.closePath();
+    this.ctx.beginPath();
+    this.ctx.font = "16px Arial";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("Score: " + this.score, 5, 16);
     this.ctx.closePath();
   }
   this.drawBricks = function() {
@@ -98,18 +150,26 @@ export const FillCanvas = function(){
     this.ctx.globalAlpha=1;    
     this.ctx.closePath();
 
-    let sides = Object.keys(this.edges);
-    if(this.edges[sides[0]][0]){
-      for(let i=0; i<sides.length; i++){
-        for(let j=0; j<this.edges[sides[i]].length; j++){
-          this.ctx.beginPath();
-          this.ctx.rect(this.edges[sides[i]][j][0], this.edges[sides[i]][j][1], 1, 1);
-          this.ctx.fillStyle = 'black';
-          this.ctx.fill();
-          this.ctx.closePath();
-        }
-      }
-    }
+    // let sides = Object.keys(this.edges);
+    // if(this.edges[sides[0]][0]){
+    //   for(let i=0; i<sides.length; i++){
+    //     for(let j=0; j<this.edges[sides[i]].length; j++){
+    //       this.ctx.beginPath();
+    //       this.ctx.rect(this.edges[sides[i]][j][0], this.edges[sides[i]][j][1], 1, 1);
+          
+    //       if(sides[i]=='top') this.ctx.fillStyle = 'black';
+    //       if(sides[i]=='right') this.ctx.fillStyle = 'red';
+    //       if(sides[i]=='bottom') this.ctx.fillStyle = 'blue';
+    //       if(sides[i]=='left') this.ctx.fillStyle = 'green';
+    //       if(sides[i]=='topLeft') this.ctx.fillStyle = 'white';
+    //       if(sides[i]=='topRight') this.ctx.fillStyle = 'orange';
+    //       if(sides[i]=='bottomLeft') this.ctx.fillStyle = 'yellow';
+    //       if(sides[i]=='bottomRight') this.ctx.fillStyle = 'grey';
+    //       this.ctx.fill();
+    //       this.ctx.closePath();
+    //     }
+    //   }
+    // }
   }
   this.drawPaddle = function() {
     this.ctx.beginPath();
@@ -125,19 +185,17 @@ export const FillCanvas = function(){
     this.drawScore();
     this.drawLives();
     this.drawBall();
-    this.drawBricks();
     this.drawPaddle();
+    this.drawBricks();
   }
   this.animate = function(){
     if(this.endGame){
       return;
     }else if(!this.endGame) {
       this.draw();
+      this.collisionDetection();
       requestAnimationFrame(this.animate);
     }
-    this.collisionDetection();
-    // this.wallCollisionDetection();
-    // this.bottomAndPaddleCollisionDetection();
   }
   this.checkEnd = function(){
     if(this.score == this.brickRowCount*this.brickColumnCount) {
@@ -185,7 +243,7 @@ export const FillCanvas = function(){
           this.removeBrick(edges.coord);
           break;
         }
-        case 'left': {
+        case 'right': {
           this.xMov = -this.xMov;
           this.removeBrick(edges.coord);
           break;
@@ -228,8 +286,9 @@ export const FillCanvas = function(){
           if(this.lives){
             this.xPos = this.canvas.width / 2;
             this.yPos = this.canvas.height-30;
-            this.xMov = 1;
-            this.yMov = -1;
+            this.setBallPixelArray(this.xPos, this.yPos)
+            this.xMov = 2;
+            this.yMov = -2;
             this.paddleX = (this.canvas.width-this.paddleWidth) / 2;
           }
           break;
@@ -262,7 +321,7 @@ export const FillCanvas = function(){
             coord: this.edges[sides[i]][j]
           }
         }else if(this.ctx.getImageData(this.edges[sides[i]][j][0], this.edges[sides[i]][j][1], 1, 1).data[3] !== 252){
-          console.log('*********', sides[i]);
+          console.log('*********', sides[i], this.edges[sides[i]][j][0], this.edges[sides[i]][j][1]);
           return {
             bounceSide: sides[i] + "",
             coord: this.edges[sides[i]][j]
@@ -274,33 +333,26 @@ export const FillCanvas = function(){
   }
 
   this.findCircleEdges = function(x, y){
-    let xTransformLeft = x - (this.ballRadius / 2);
-    let xTransformRight = x + (this.ballRadius / 2);
-    let yTransformDown = y + (this.ballRadius / 2);
-    let yTransformUp = y - (this.ballRadius / 2);
-    this.edges.topLeft[0] = [xTransformLeft, yTransformUp];
-    this.edges.bottomLeft[0] = [xTransformLeft, yTransformDown];
-    this.edges.bottomRight[0] = [xTransformRight, yTransformDown];
-    this.edges.topRight[0] = [xTransformRight, yTransformUp];
-
-    for(let i = -3; i<3; i++){
-      this.edges.bottom[i+3] = [(x+i), ( y+(this.ballRadius-2) )];
-      this.edges.bottom[i+8] = [(x+i), ( y+(this.ballRadius-3) )];
-      this.edges.bottom[i+13] = [(x+i), ( y+(this.ballRadius-4) )];
-
-      this.edges.top[i+3] = [(x+i), ( y-(this.ballRadius-2) )];
-      this.edges.top[i+8] = [(x+i), ( y-(this.ballRadius-3) )];
-      this.edges.top[i+13] = [(x+i), ( y-(this.ballRadius-4) )];
-
-      this.edges.right[i+3] = [( x+(this.ballRadius-2) ), (y+i)];
-      this.edges.right[i+8] = [( x+(this.ballRadius-3) ), (y+i)];
-      this.edges.right[i+13] = [( x+(this.ballRadius-4) ), (y+i)];
-
-      this.edges.left[i+3] = [( x-(this.ballRadius-2) ), (y+i)];
-      this.edges.left[i+8] = [( x-(this.ballRadius-3) ), (y+i)];
-      this.edges.left[i+13] = [( x-(this.ballRadius-4) ), (y+i)];
+    for(let i=0; i<this.edges.top.length; i++){
+      this.edges.top[i][0] += this.xMov;
+      this.edges.right[i][0] += this.xMov;
+      this.edges.left[i][0] += this.xMov;
+      this.edges.bottom[i][0] += this.xMov;
+      this.edges.top[i][1] += this.yMov;
+      this.edges.right[i][1] += this.yMov;
+      this.edges.left[i][1] += this.yMov;
+      this.edges.bottom[i][1] += this.yMov;
+      if(i>2) continue;
+      this.edges.topRight[i][1] += this.yMov;
+      this.edges.topLeft[i][1] += this.yMov;
+      this.edges.bottomRight[i][1] += this.yMov;
+      this.edges.bottomLeft[i][1] += this.yMov;
+      this.edges.topRight[i][0] += this.xMov;
+      this.edges.topLeft[i][0] += this.xMov;
+      this.edges.bottomLeft[i][0] += this.xMov;
+      this.edges.bottomRight[i][0] += this.xMov;
     }
-
+    
   }
 
   this.draw = this.draw.bind(this);
