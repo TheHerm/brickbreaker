74fe36dcdbcb14ca7300d06ec7041c495653341a
Alan H <alan.herman@outlook.com>
2017-04-24 17:31:49 -0500

    cached holding pattern when brick is initialized, now when brick is close to home it enters holding pattern

--- client/bricks.jsx
+++ client/bricks.jsx
@@ -7,13 +7,8 @@ export  const Brick = function(id){
   this.y = 0;
   this.xMov = 50;
   this.yMov = 0;
-  this.rowCount = 3;
-  this.columnCount = 12;
   this.width = 50;
   this.height = 70;
-  this.padding = 5;
-  this.offsetTop = 50;
-  this.offsetLeft = 47;
   this.dead = false;
   this.ballDist = 1000;
   this.paddleDist = 1000;
@@ -30,8 +25,10 @@ export  const Brick = function(id){
   this.xEscape = 1;
   this.yEscape = -1;
   this.lastBallDist = 0;
+  this.holdingPattern = [0];
+  this.holdingPatternSpot = 0;
+  this.holdingPatternLen = 0;
   this.circleRadius = (Math.random() * 10000000) % 60 + 15;
-  this.circleDirection = 0;
 
 /* -------------- FUNCTIONS ----------------*/
 
@@ -41,13 +38,46 @@ export  const Brick = function(id){
     this.setActiveWidthAndHeight(width, height);
     this.initialX = Math.round((this.activeWidth[1] - 5) * Math.random() + 5);
     this.initialY = Math.round((this.activeHeight[1] - 5) * Math.random() + 5);
-    this.x = this.initialX;
-    this.y = this.initialY;
-    Math.random() >= .5 ? this.circleDirection = 1 : this.circleDirection = -1; 
+    this.findHoldingPattern();
+    this.x = this.holdingPattern[1][0];
+    this.y = this.holdingPattern[1][1];
+    this.holdingPatternLen = this.holdingPattern.length;
   }
   this.setActiveWidthAndHeight = function(width, height){
     this.activeHeight = [5, Math.round(height + 50 * Math.random())];
     this.activeWidth = [Math.round(this.width+this.width*.3*Math.random()), Math.round(width-this.width-(Math.random()*this.width*.7))];
+  }
+  this.findHoldingPattern = function(){
+    let opp=0, adj=0, circleAngle=0, circleMove, circleRadius = this.circleRadius, tempAngle;
+    Math.random() >= .5 ? circleMove = -20 * this.personality : circleMove = 20 * this.personality;
+    this.holdingPattern[1] = [Math.round(this.initialX + circleRadius), this.initialY];
+    circleAngle += circleMove;
+    while( !( circleAngle > -Math.abs(circleMove) && circleAngle < Math.abs(circleMove) ) ){
+      if(circleAngle >= 180){
+        circleAngle = -(180 - (circleAngle - 180));
+      }else if(circleAngle <= -180){
+        circleAngle = 180 - (Math.abs(circleAngle) - 180);
+      }
+      if(circleAngle > 90) {
+        tempAngle = circleAngle - 90;
+        opp = Math.sin(tempAngle) * -circleRadius;
+        adj = Math.cos(tempAngle) * -circleRadius;
+      }else if(circleAngle <= 90 && circleAngle >= 0){
+        opp = Math.sin(circleAngle) * -circleRadius;
+        adj = Math.cos(circleAngle) * circleRadius;
+      }else if(circleAngle < -90){
+        tempAngle = Math.abs(circleAngle) - 90;
+        opp = Math.sin(tempAngle) * circleRadius;
+        adj = Math.cos(tempAngle) * -circleRadius;
+      }else if(circleAngle < 0 && circleAngle >= -90){
+        tempAngle = circleAngle * -1;
+        opp = Math.sin(tempAngle) * circleRadius;
+        adj = Math.cos(tempAngle) * circleRadius;
+      }
+      this.holdingPattern.push([Math.round(this.initialX + adj), Math.round(this.initialY + opp)]);        
+      circleAngle += circleMove;
+    }
+    if(this.id === 1)console.log(this.holdingPattern);
   }
 
       /* --------- behavior ---------*/
@@ -108,9 +138,17 @@ export  const Brick = function(id){
     return false;
   }
   this.circleHome = function(){
-    
-    this.setX(undefined, );
-    this.setY(undefined, );
+    this.circleAngle = (this.circleDirection * 5) + Math.atan2((this.initialY - this.y), (this.x - this.initialX)) * 180 / Math.PI;
+    this.circleAngle += (this.circleDirection * 5);
+    if(this.circleAngle >= 180){
+      this.circleAngle = -(180 - (this.circleAngle - 180));
+    }else if(this.circleAngle <= -180){
+      this.circleAngle = 180 - (Math.abs(this.circleAngle) - 180);
+    }
+
+    if(this.id === 1) console.log(this.circleAngle);
+    this.setX(undefined, undefined);
+    this.setY(undefined, undefined);
   }
   this.stepForward = function(ballX, ballY, paddleX, paddleY){
     this.ballDist = Math.sqrt(Math.pow(ballX-this.x, 2)+Math.pow(ballY-this.y, 2));
@@ -123,7 +161,15 @@ export  const Brick = function(id){
         this.setX(undefined, (-this.moveDirectionLR * this.distFromHome / 100) );
         this.setY(undefined, (-this.moveDirectionUD * this.distFromHome / 100) );
       }else {
-        this.circleHome();
+        if(this.holdingPatternSpot === this.holdingPatternLen - 1) this.holdingPatternSpot = 0;
+        if(this.holdingPattern[0] === 7) {
+          this.holdingPatternSpot++;
+          this.holdingPattern[0] = -1;
+        }
+        this.holdingPattern[0]++;
+        if(this.id === 1) console.log(this.holdingPatternSpot, this.holdingPattern[0], this.holdingPatternLen);
+        this.setX(this.holdingPattern[this.holdingPatternSpot][0], 0)
+        this.setY(this.holdingPattern[this.holdingPatternSpot][1], 0)
       }
     }else{
       if(this.restrainXMovement()) return;
