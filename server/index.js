dc93938ba7cb49504cb7db9ca9daba0a5c91f8c4
Alan H <alan.herman@outlook.com>
2017-05-02 19:01:15 -0500

    changed server port

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ var app = express();
 
 server.on('request', app);
 
-server.listen(4567, function () {
-    console.log('The server is listening on port 4567!');
+server.listen(80, function () {
+    console.log('The server is listening on port 80!');
 });
 
 app.use(express.static('public'));
